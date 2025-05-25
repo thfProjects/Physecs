fd6e6535cfb77ecf1cd6d0cb3bcf683e8e5cbbee
deccer <decccer@outlook.com>
2025-05-25 03:47:23 +0200

    Extend PHYSECS_API for non msvc compilers

--- include/Physecs/PhysecsAPI.h
+++ include/Physecs/PhysecsAPI.h
@@ -1,7 +1,11 @@
 #pragma once
 
 #ifdef PHYSECS_EXPORTS
-    #define PHYSECS_API __declspec(dllexport)
+    #if defined(_MSC_VER)
+        #define PHYSECS_API __declspec(dllexport)
+    #elif defined(__GNUC__)
+        #define PHYSECS_API __attribute__((visibility("default")))
+    #endif
 #else
     #define PHYSECS_API
 #endif

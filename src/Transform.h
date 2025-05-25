a21876b4009fd0e9a664fc8dee75cfa61b09ac5c
deccer <decccer@outlook.com>
2025-05-25 03:44:34 +0200

    Fix glm includes

--- src/Transform.h
+++ src/Transform.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <glm\glm.hpp>
-#include <glm\gtx\quaternion.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtx/quaternion.hpp>
 
 struct TransformComponent {
     glm::vec3 position;

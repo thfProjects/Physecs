42693eaa07b24938d56a968da83b017c47111eb9
Nikola Djurovic <nikola.djurovic997@gmail.com>
2026-01-04 14:09:27 +0100

    JointSolverData::calculateWorldSpaceData optimize

--- src/Joint.cpp
+++ src/Joint.cpp
@@ -3,15 +3,12 @@
 #include "Transform.h"
 
 void physecs::JointSolverData::calculateWorldSpaceData(JointWorldSpaceData& data) const {
-    const glm::mat3 rot0 = glm::toMat3(transform0.orientation);
-    const glm::mat3 rot1 = glm::toMat3(transform1.orientation);
+    data.u0 = glm::toMat3(transform0.orientation * anchor0Or);
+    data.u1 = glm::toMat3(transform1.orientation * anchor1Or);
 
-    data.u0 = rot0 * glm::toMat3(anchor0Or);
-    data.u1 = rot1 * glm::toMat3(anchor1Or);
+    data.r0 = transform0.orientation * r0;
+    data.r1 = transform1.orientation * r1;
 
-    data.r0 = rot0 * r0;
-    data.r1 = rot1 * r1;
-
-    data.p0 = transform0.position + rot0 * anchor0Pos;
-    data.p1 = transform1.position + rot1 * anchor1Pos;
+    data.p0 = transform0.position + transform0.orientation * anchor0Pos;
+    data.p1 = transform1.position + transform1.orientation * anchor1Pos;
 }
